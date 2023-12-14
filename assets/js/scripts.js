4ff19816d00bbb969313dba495908ad623c1e78b
Basil Barnaby <basil.barnaby@devmassive.com>
2023-12-13 22:57:21 -0500

    Fixed flight detail bugs
    Added comments
    
    Added footer with student info and API documentation link

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,6 @@
-/** Variables */
+/** Variable Declaration */
+const studentName = document.querySelector("#student-name");
+const studentId = document.querySelector("#student-id");
 const apiKey = "38c7d77d40ecff4ba100b9a4fd0a182d";
 const baseUrl = "http://api.aviationstack.com/v1/flights";
 const searchContainer = document.querySelector("#search-container");
@@ -13,13 +15,21 @@ const detailsContainer = document.querySelector("#details-container");
 const flightIata = document.querySelector(".flight-iata");
 const airlineName = document.querySelector(".airline-name");
 const departureIata = document.querySelector(".departure-iata");
-const departureIataSecondary = document.querySelector(".departure-iata-secondary");
+const departureIataSecondary = document.querySelector(
+  ".departure-iata-secondary"
+);
 const departureIcao = document.querySelector(".departure-icao");
 const departureAirport = document.querySelector(".departure-airport-name");
+const departureAirportSecondary = document.querySelector(
+  ".departure-airport-name-secondary"
+);
 const arrivalIata = document.querySelector(".arrival-iata");
 const arrivalIataSecondary = document.querySelector(".arrival-iata-secondary");
 const arrivalIcao = document.querySelector(".arrival-icao");
 const arrivalAirport = document.querySelector(".arrival-airport-name");
+const arrivalAirportSecondary = document.querySelector(
+  ".arrival-airport-name-secondary"
+);
 const flightStatus = document.querySelector(".flight-status");
 const departureScheduled = document.querySelector(".departure-scheduled");
 const departureEstimated = document.querySelector(".departure-estimated");
@@ -34,12 +44,20 @@ const arrivalRunway = document.querySelector(".arrival-runway");
 const arrivalTerminal = document.querySelector(".arrival-terminal");
 const arrivalGate = document.querySelector(".arrival-gate");
 
+// Populate student details
+studentName.textContent = "Basil Barnaby";
+studentId.textContent = "200540109";
+
+// Function that gets the flights from the API with the given parameters
 function getFlights() {
   flightTableContainer.classList.add("hidden");
+
+  // Get the values from the input fields
   let departure = departureInput.value;
   let arrival = arrivalInput.value;
   let airline = airlineInput.value;
 
+  // Check if the mandatory field is empty and alert the user otherwise make the API call
   if (departure === "") {
     alert("Please enter a departure airport code");
   } else {
@@ -54,6 +72,7 @@ function getFlights() {
   }
 }
 
+// Function that populates the table with the flights data
 function populateFlightsData(data) {
   console.log(data.data);
   tableBody.innerHTML = "";
@@ -64,6 +83,7 @@ function populateFlightsData(data) {
     let flightsArray = data.data;
 
     for (let i = 0; i < flightsArray.length; i++) {
+      // Create the table elements
       let tableTr = document.createElement("tr");
       let airlineTh = document.createElement("th");
       let flightNumberTd = document.createElement("td");
@@ -85,7 +105,6 @@ function populateFlightsData(data) {
         "text-gray-900",
         "whitespace-nowrap"
       );
-
       flightNumberTd.classList.add("px-6", "py-4");
       departureTd.classList.add("px-6", "py-4");
       arrivalTd.classList.add("px-6", "py-4");
@@ -102,6 +121,7 @@ function populateFlightsData(data) {
         "hover:bg-blue-700"
       );
 
+      // Populate the table elements with the data
       airlineTh.textContent = flightsArray[i].airline.name;
       flightNumberTd.textContent = flightsArray[i].flight.iata;
       departureTd.textContent = flightsArray[i].departure.airport;
@@ -121,6 +141,7 @@ function populateFlightsData(data) {
 
       actionTd.appendChild(viewButton);
 
+      // Append the table elements to the table body
       tableTr.appendChild(airlineTh);
       tableTr.appendChild(flightNumberTd);
       tableTr.appendChild(departureTd);
@@ -129,13 +150,16 @@ function populateFlightsData(data) {
       tableTr.appendChild(statusTd);
       tableTr.appendChild(actionTd);
 
+      // Append the table row to the table body
       tableBody.appendChild(tableTr);
     }
 
+    // Show the table
     flightTableContainer.classList.remove("hidden");
   }
 }
 
+// Function that gets the flight details from the API with the given parameters
 function getFlight(
   departureIata,
   arrivalIata,
@@ -143,8 +167,7 @@ function getFlight(
   flightNumber,
   flightStatus
 ) {
-  // const url = `http://api.aviationstack.com/v1/flights?access_key=${apiKey}&dep_iata=${departureIata}&arr_iata=${arrivalIata}&airline_iata=${airlineIata}&flight_number=${flightNumber}&flight_status=${flightStatus}`;
-
+  // Check if the mandatory params are empty and alert the user otherwise make the API call
   if (
     departureIata == null ||
     arrivalIata == null ||
@@ -166,6 +189,7 @@ function getFlight(
   }
 }
 
+// Function that populates the flight details
 function populateFlightData(data) {
   console.log(data.data);
   if (data && data.data.length > 0) {
@@ -173,13 +197,15 @@ function populateFlightData(data) {
     flightIata.textContent = data.data[0].flight.iata;
     airlineName.textContent = data.data[0].airline.name;
     departureIata.textContent = data.data[0].departure.iata;
-	departureIataSecondary.textContent = `IATA: ${data.data[0].departure. iata}`;
+    departureIataSecondary.textContent = `IATA: ${data.data[0].departure.iata}`;
     departureIcao.textContent = data.data[0].departure.icao;
     departureAirport.textContent = data.data[0].departure.airport;
+    departureAirportSecondary.textContent = data.data[0].departure.airport;
     arrivalIata.textContent = data.data[0].arrival.iata;
-	arrivalIataSecondary.textContent = `IATA: ${data.data[0].arrival.iata}`;
+    arrivalIataSecondary.textContent = `IATA: ${data.data[0].arrival.iata}`;
     arrivalIcao.textContent = `ICAO: ${data.data[0].arrival.icao}`;
     arrivalAirport.textContent = data.data[0].arrival.airport;
+    arrivalAirportSecondary.textContent = data.data[0].arrival.airport;
     flightStatus.textContent = data.data[0].flight_status;
 
     departureScheduled.textContent = formatDate(
@@ -201,6 +227,7 @@ function populateFlightData(data) {
   }
 }
 
+// Function that formats the date
 function formatDate(dateString) {
   const options = {
     year: "numeric",
@@ -212,4 +239,5 @@ function formatDate(dateString) {
   return new Date(dateString).toLocaleDateString(undefined, options);
 }
 
+// Event listener for the search button
 searchBtn.addEventListener("click", getFlights);
